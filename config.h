4d71b4c87753011ca6bbcf0da840a5c31e4c3789
AtomToast <reg-2@t-online.de>
2019-11-27 23:05:25 +0100

    apply scrollback patch
    apply st-scrollback-20190331-21367a0.diff

--- config.h
+++ config.h
@@ -171,6 +171,7 @@ static MouseShortcut mshortcuts[] = {
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD2 (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -183,9 +184,13 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ TERMMOD,              XK_P,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+    { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+    { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+    { TERMMOD2,               XK_Y,           kscrollup,      {.i = -1} },
+    { TERMMOD2,               XK_E,           kscrolldown,    {.i = -1} },
 };
 
 /*
