ac1aace46886b9466ffa8e2988ff67a2f5437e90
AtomToast <reg-2@t-online.de>
2019-11-27 23:36:36 +0100

    add newterm patch

--- config.def.h
+++ config.def.h
@@ -192,6 +192,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_P,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+    { TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
     { TERMMOD2,             XK_Y,           kscrollup,      {.i = -1} },
