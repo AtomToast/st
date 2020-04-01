8a757c6bea66f6cd61b506ecd5e3ac4b96468cc3
AtomToast <reg-2@t-online.de>
2020-04-01 14:56:59 +0200

    change default cursor shape to bar

--- config.h
+++ config.h
@@ -164,7 +164,7 @@ static unsigned int defaultrcs = 0; //257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
