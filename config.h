47a078793643efcba231892dd738176becab87f4
AtomToast <reg-2@t-online.de>
2020-05-05 20:48:43 +0200

    update colorscheme to fit crusoexia's
    updated my vim colorscheme to the updated monokai version of crusoexia.
    So now I am updating my st colors aswell

--- config.h
+++ config.h
@@ -91,21 +91,21 @@ unsigned int tabspaces = 8;
 
 /* Base16 Monokai colors - Wimer Hazenberg (http://www.monokai.nl) */
 static const char base00[] = "#282828"; // "#272822"
-static const char base01[] = "#383830";
-static const char base02[] = "#49483e";
-static const char base03[] = "#75715e";
-static const char base04[] = "#a59f85";
-static const char base05[] = "#f8f8f2";
-static const char base06[] = "#f5f4f1";
-static const char base07[] = "#f9f8f5";
-static const char base08[] = "#f92672";
-static const char base09[] = "#fd971f";
-static const char base0A[] = "#f4bf75";
-static const char base0B[] = "#a6e22e";
-static const char base0C[] = "#a1efe4";
-static const char base0D[] = "#66d9ef";
-static const char base0E[] = "#ae81ff";
-static const char base0F[] = "#cc6633";
+static const char base01[] = "#e73c50";
+static const char base02[] = "#a6e22e";
+static const char base03[] = "#e6db74";
+static const char base04[] = "#66d9ef";
+static const char base05[] = "#ae81ff";
+static const char base06[] = "#a1efe4";
+static const char base07[] = "#e8e8e3";
+static const char base08[] = "#64645e";
+static const char base09[] = "#f92772";
+static const char base0A[] = "#9ec400";
+static const char base0B[] = "#e7c547";
+static const char base0C[] = "#7aa6da";
+static const char base0D[] = "#b77ee0";
+static const char base0E[] = "#54ced6";
+static const char base0F[] = "#ffffff";
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -136,22 +136,22 @@ static const char *colorname[] = {
 	/* "#555555", */
 
     base00,
-    base08,
-    base0B,
-    base0A,
-    base0D,
-    base0E,
-    base0C,
-    base05,
-
+    base01,
+    base02,
     base03,
+    base04,
+    base05,
+    base06,
+    base07,
+
     base08,
-    base0B,
+    base09,
     base0A,
+    base0B,
+    base0C,
     base0D,
     base0E,
-    base0C,
-    base07
+    base0F
 };
 
 
